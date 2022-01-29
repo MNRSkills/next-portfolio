d1e4dbeef4979bd8a11570d9878bf6a9c5e80e83
MNR_Dev <mnrdev510@gmail.com>
2022-01-29 08:59:15 -0800

    added components & layout

--- pages/blog/[posts].js
+++ pages/blog/[posts].js
@@ -1,7 +1,11 @@
+import {useRouter} from "next/router";
+import Link from "next/link";
+
 const PostDetail = () => {
   return (
     <div>
       <h1>This is the PoSt Page in Dynamic form</h1>
+      <Link href="/">Home</Link>
     </div>
   );
 };
