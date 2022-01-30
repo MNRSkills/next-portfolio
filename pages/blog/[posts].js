cdb1b71aed070a87863322891f6e24aa2afbf60a
MNR_Dev <mnrdev510@gmail.com>
2022-01-29 19:10:25 -0800

    adding animation organized files

--- pages/blog/[posts].js
+++ pages/blog/[posts].js
@@ -1,9 +1,10 @@
 import {useRouter} from "next/router";
 import Link from "next/link";
+import PostStyles from "../../styles/BlogPost.module.css";
 
 const PostDetail = () => {
   return (
-    <div>
+    <div className={PostStyles.postContainer}>
       <h1>This is the PoSt Page in Dynamic form</h1>
       <Link href="/">Home</Link>
     </div>
