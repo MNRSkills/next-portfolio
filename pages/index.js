bc810de695d170b93cd25d70f7ebc877ff52ac3e
MNR_Dev <mnrdev510@gmail.com>
2022-01-30 06:27:39 -0800

    adding framer motion removed READme

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import { motion } from "framer-motion";
+import HomeStyles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
-    <div>
-      <main>
+    <motion.div animate={{y: 100}} transition={{ ease:"easeOut", duration: 2}}>
+      <main className={HomeStyles.home}>
         <h1>Mack Ramirez Ready To Work</h1>
       </main>
-    </div>
+    </motion.div>
   );
 }
