de53cf2eee23771ba3132d5b356de0ee07d98c27
MNR_Dev <mnrdev510@gmail.com>
2022-03-11 20:05:37 -0800

    let prettier be pretty

--- components/Projects/projectDetail.js
+++ components/Projects/projectDetail.js
@@ -6,12 +6,17 @@ import projectStyles from "../../styles/ProjectCard.module.css";
 const ProjectDetail = (props) => {
   console.log("LOOKING FOR PROPS", props.project.fields);
 
-  const {title, slug, thumbnailUrl, bannerUrl, repoUrl, siteUrl} = props.project.fields;
- 
+  const { title, slug, thumbnailUrl, bannerUrl, repoUrl, siteUrl } =
+    props.project.fields;
+
   return (
     <div className={projectStyles.card}>
       <h1>{title}</h1>
-      <Image src={"https:" + thumbnailUrl.fields.file.url} width="200px" height="200px"/>     
+      <Image
+        src={"https:" + thumbnailUrl.fields.file.url}
+        width='200px'
+        height='200px'
+      />
     </div>
   );
 };
