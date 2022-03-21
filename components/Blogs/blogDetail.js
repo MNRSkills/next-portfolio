e01bfa4b6d1463e031dc84f758a2699ef06b1c8f
MNR_Dev <mnrdev510@gmail.com>
2022-03-20 22:02:35 -0700

    minor changes to detail page

--- components/Blogs/blogDetail.js
+++ components/Blogs/blogDetail.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import slugStyles from "../../styles/BlogDetail.module.css";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 const BlogDetail = (props) => {
-    console.log(props)
-    // const { title, } = props.blogPost.fields;
+  console.log(props);
+  const { title, subtitle, bannerImage, blogContent } = props.blogPost.fields;
   return (
-    <div>BlogDetail</div>
-  )
-}
+    <div>
+      <div className={slugStyles.blogCard}>
+        <h1>{title}</h1>
+        <h6>{subtitle}</h6>
+        <Image
+          src={"https:" + bannerImage.fields.file.url}
+          width={bannerImage.fields.file.details.image.width}
+          height={bannerImage.fields.file.details.image.height}
+        />
+        <div> 
+          {documentToReactComponents(blogContent)}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BlogDetail;
